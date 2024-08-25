ee49b6877e624e16a7e875550847052c96cd855b
Huijun Seo <gjshj366@gmail.com>
2024-08-26 03:23:06 +0900

    Feat: #3 - 이메일 전송 서비스 구축

---
+++ src/main/java/com/now_here5/now_here/infra/email/service/EmailCodeService.java
@@ -0,0 +1,72 @@
+package com.now_here5.now_here.infra.email.service;
+
+
+import com.now_here5.now_here.global.util.RandomNumberUntil;
+import com.now_here5.now_here.infra.email.dto.EmailRequestDto;
+import com.now_here5.now_here.infra.email.repository.EmailMemoryRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class EmailCodeService {
+
+    private static final String REGISTER_EMAIL_SUBJECT_POSTFIX = " 님의 소중한 의견 감사합니다.!";
+
+    private final EmailMemoryRepository emailMemoryRepository;
+    private final EmailService emailService;
+    private String confirmEmailUI;
+
+    public boolean sendRegisterCode(EmailRequestDto emailRequestDto) {
+        try {
+            String randomCode= RandomNumberUntil.generateRandomCode();
+
+            emailService.sendHtmlTextEmail(
+                    emailRequestDto.getNickname() + REGISTER_EMAIL_SUBJECT_POSTFIX,
+                    getRegisterEmailText(emailRequestDto.getEmail(), randomCode),
+                    emailRequestDto.getEmail()); // 메일 전송
+
+            emailMemoryRepository.saveCheckCode(emailRequestDto.getEmail(), randomCode); // 랜덤 번호, 이메일 저장.
+
+            return true;
+
+        }catch (Exception e){
+            log.error("Error sending email code: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean checkRegisterCode( String email, String code) {
+        final String savedCode = emailMemoryRepository.findCheckCodeByEmail(email);
+
+        boolean result = (savedCode != null) && (savedCode.equals(code));
+        emailMemoryRepository.deleteEmail(email);
+        return result;
+    }
+
+
+    private String getRegisterEmailText(String email, String code) {
+        return String.format(confirmEmailUI, email, code);
+    }
+
+
+    @PostConstruct
+    private void loadEmailUI() throws Exception {
+        try {
+            final ClassPathResource confirmEmailUIResource = new ClassPathResource("static/confirmEmailUI.html");
+            confirmEmailUI = new String(confirmEmailUIResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+        } catch (IOException e) {
+            log.error("Error loading email UI template: {}", e.getMessage()); // 예외 발생 시 로그 출력
+            throw new Exception("Error loading email UI template", e);
+        }
+    }
+}
