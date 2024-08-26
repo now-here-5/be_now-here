d8f4180bc175e729775f25ba291ac071b2e50848
Huijun Seo <gjshj366@gmail.com>
2024-08-27 04:21:45 +0900

    Chore : #3 - 스웨거 토큰 옵션 설정

--- src/main/java/com/now_here5/now_here/global/config/SecurityConfig.java
+++ src/main/java/com/now_here5/now_here/global/config/SecurityConfig.java
@@ -47,8 +47,8 @@ public class SecurityConfig {
                                 // 권한 없이 접근 가능한 URL 설정
 
                                 // 매니저 대상
-                                .requestMatchers("/swagger-ui/**").hasRole(RoleName.ADMIN.name())
-                                .requestMatchers("/v3/api-docs/**").hasRole(RoleName.ADMIN.name())
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 
                                 // 비유저 대상
                                 .requestMatchers(HttpMethod.GET, "/event/**").permitAll()
