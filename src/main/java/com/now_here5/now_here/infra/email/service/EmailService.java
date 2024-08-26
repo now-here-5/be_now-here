21f48e65d3c586348f816a601d3b727173a8e943
Huijun Seo <gjshj366@gmail.com>
2024-08-26 22:41:21 +0900

    Chore: #3 - 이메일 서비스 주석

--- src/main/java/com/now_here5/now_here/infra/email/service/EmailService.java
+++ src/main/java/com/now_here5/now_here/infra/email/service/EmailService.java
@@ -1,38 +1,38 @@
-package com.now_here5.now_here.infra.email.service;
-
-
-
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-
-    private static final String ENCODING_UTF8 = "UTF-8";
-
-    private final JavaMailSender javaMailSender;
-
-    @Async
-    public void sendHtmlTextEmail(String subject, String content, String email) {
-        final MimeMessage message = javaMailSender.createMimeMessage();
-        try {
-            final MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, ENCODING_UTF8);
-            messageHelper.setTo(email);
-            messageHelper.setSubject(subject);
-            messageHelper.setText(content, true);
-            javaMailSender.send(message);
-
-        } catch (Exception e) {
-            log.error("error on sending a mail : {}", e.getMessage()); // 예외 발생 시 로그 출력
-            throw new RuntimeException() ;
-        }
-    }
-
-}
+//package com.now_here5.now_here.infra.email.service;
+//
+//
+//
+//import jakarta.mail.internet.MimeMessage;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+//import org.springframework.scheduling.annotation.Async;
+//import org.springframework.stereotype.Service;
+//
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//public class EmailService {
+//
+//    private static final String ENCODING_UTF8 = "UTF-8";
+//
+//    private final JavaMailSender javaMailSender;
+//
+//    @Async
+//    public void sendHtmlTextEmail(String subject, String content, String email) {
+//        final MimeMessage message = javaMailSender.createMimeMessage();
+//        try {
+//            final MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, ENCODING_UTF8);
+//            messageHelper.setTo(email);
+//            messageHelper.setSubject(subject);
+//            messageHelper.setText(content, true);
+//            javaMailSender.send(message);
+//
+//        } catch (Exception e) {
+//            log.error("error on sending a mail : {}", e.getMessage()); // 예외 발생 시 로그 출력
+//            throw new RuntimeException() ;
+//        }
+//    }
+//
+//}
