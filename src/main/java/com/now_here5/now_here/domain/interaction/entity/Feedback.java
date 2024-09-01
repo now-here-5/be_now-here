c7fb1ee660d06c0f5de499576dabd17ed22bfef4
jun10920 <abide0206@naver.com>
2024-09-01 19:27:39 +0900

    :art: Fix: 피드백 생성시 null값 허용되게 수정

--- src/main/java/com/now_here5/now_here/domain/interaction/entity/Feedback.java
+++ src/main/java/com/now_here5/now_here/domain/interaction/entity/Feedback.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class Feedback extends Interaction{
 
     @Column(name = "field", nullable = true)
-    @Min(1)  // 최소값 1
+//    @Min(1)  // 최소값 1
     @Max(5)  // 최대값 5
     private int field;  // 별점
 
