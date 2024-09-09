33f59b5c7afdaec0b2699ea65f8ee9e2cdc13c77
Huijun Seo <gjshj366@gmail.com>
2024-09-09 20:24:31 +0900

    Infra : 키 주입 테스트 4

--- src/main/java/com/now_here5/now_here/infra/notification/config/FcmConfig.java
+++ src/main/java/com/now_here5/now_here/infra/notification/config/FcmConfig.java
@@ -21,32 +21,27 @@ public class FcmConfig {
     @Value("${fcm.credentials.path}")
     private String serviceAccountKeyPath;
 
-    @Value("${java.encode:false}")  // 기본값을 false로 설정
-    private boolean isJavaEncode;
+    @Value("${fcm.service.account.key:}")  // 기본값을 false로 설정
+    private String serviceAccountKeyBase64;
 
     @PostConstruct
     public void initialize() {
         try {
-            if (isJavaEncode) {
+            if (!serviceAccountKeyBase64.isEmpty()) {
                 // 환경 변수에서 Base64 인코딩된 FCM 서비스 계정 키 가져오기
-                String serviceAccountKeyBase64 = System.getenv("FCM_SERVICE_ACCOUNT_KEY");
 
-                if (serviceAccountKeyBase64 != null) {
-                    // Base64 디코딩 후 ByteArrayInputStream으로 변환
-                    byte[] decodedBytes = java.util.Base64.getDecoder().decode(serviceAccountKeyBase64);
-                    ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(decodedBytes);
+                // Base64 디코딩 후 ByteArrayInputStream으로 변환
+                byte[] decodedBytes = java.util.Base64.getDecoder().decode(serviceAccountKeyBase64.replaceAll("\\s", ""));
+                ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(decodedBytes);
 
-                    // Firebase 옵션 설정
-                    GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccountStream);
-                    FirebaseOptions options = FirebaseOptions.builder()
-                            .setCredentials(googleCredentials)
-                            .build();
+                // Firebase 옵션 설정
+                GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccountStream);
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(googleCredentials)
+                        .build();
 
-                    FirebaseApp.initializeApp(options);
-                    log.info("FirebaseApp successfully initialized using Base64-encoded key.");
-                } else {
-                    throw new IllegalStateException("FCM Base64 인코딩된 서비스 계정 키를 찾을 수 없습니다.");
-                }
+                FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp successfully initialized using Base64-encoded key.");
             } else {
                 // 파일 경로에서 Firebase 서비스 계정 키 로드
                 if (serviceAccountKeyPath != null && !serviceAccountKeyPath.isEmpty()) {
