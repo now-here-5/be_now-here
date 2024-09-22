dcf173694a5544d7db5db085f68be3491e85f703
Huijun Seo <gjshj366@gmail.com>
2024-09-22 15:16:21 +0900

    Chore : 로깅 설정

--- src/main/java/com/now_here5/now_here/global/logging/exception/GlobalExceptionHandler.java
+++ src/main/java/com/now_here5/now_here/global/logging/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.now_here5.now_here.global.logging;
+package com.now_here5.now_here.global.logging.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
