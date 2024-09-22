656c48ae30b24684bfb98dbc29e95089c793dc72
Huijun Seo <gjshj366@gmail.com>
2024-09-22 18:58:54 +0900

    Chore : 로깅 null 처리

--- src/main/java/com/now_here5/now_here/global/logging/aop/DefaultLoggingAspect.java
+++ src/main/java/com/now_here5/now_here/global/logging/aop/DefaultLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.now_here5.now_here.global.logging.aop;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,13 @@ import org.springframework.stereotype.Component;
 public class DefaultLoggingAspect {
 
     private final HttpServletRequest request;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Around("execution(* com.now_here5.now_here.domain.*.*(..))")
+    @Around("execution(* com.now_here5.now_here.domain.member.controller.MemberController.*(..)) || " +
+            "execution(* com.now_here5.now_here.domain.member.controller.MemberInfoController.*(..)) || " +
+            "execution(* com.now_here5.now_here.domain.event.controller.EventController.*(..)) || " +
+            "execution(* com.now_here5.now_here.domain.interaction.controller.InteractionController.*(..)) || " +
+            "execution(* com.now_here5.now_here.domain.matching.controller.MatchingController.*(..))")
     public Object logAPIRequests(ProceedingJoinPoint joinPoint) throws Throwable {
         // 요청 정보 로깅
         String httpMethod = request.getMethod();
@@ -29,15 +35,15 @@ public class DefaultLoggingAspect {
         Object result = null;
         try {
             result = joinPoint.proceed();  // 실제 메서드 실행
-            return result;
         } catch (Exception e) {
-            log.error("❗Error occurred: ", e);
-            throw e;
+            logError(e, requestUrl, clientIp);
+            throw e;  // 예외를 다시 던져서 처리
         } finally {
             long duration = System.currentTimeMillis() - startTime;
             log.info("⏱️ Request to: {} completed in {} ms", joinPoint.getSignature().toShortString(), duration);
-            log.info("📤 Response: {}", result);
+            logResponse(result);
         }
+        return result;
     }
 
     // 클라이언트의 IP 주소를 가져오는 메서드
@@ -47,5 +53,23 @@ public class DefaultLoggingAspect {
             ipAddress = request.getRemoteAddr();
         }
         return ipAddress;
+    }
+
+    // 에러 로깅 메서드
+    private void logError(Exception e, String requestUrl, String clientIp) {
+        log.error("❗ Error occurred while processing request to {} from IP {}: {}", requestUrl, clientIp, e.getMessage(), e);
+    }
+
+    // 응답 로깅 메서드
+    private void logResponse(Object result) {
+        if (result == null) {
+            log.info("📤 Response: No response (null)");
+            return;
+        }
+        try {
+            log.info("📤 Response: {}", objectMapper.writeValueAsString(result));
+        } catch (Exception e) {
+            log.error("Error serializing response to JSON: {}", e.getMessage());
+        }
     }
 }
--- src/main/java/com/now_here5/now_here/global/security/filter/CustomAuthFilter.java
+++ src/main/java/com/now_here5/now_here/global/security/filter/CustomAuthFilter.java
@@ -45,7 +45,7 @@ public class CustomAuthFilter extends GenericFilterBean {
         String customToken = resolveToken(httpServletRequest);
 
         String requestURI = httpServletRequest.getRequestURI();
-
+        log.info("resolveToken : {}, requestURI : {} ", customToken, requestURI);
 
         if (StringUtils.hasText(customToken)) { // 토큰이 있을 때만 검증
             try {
@@ -69,7 +69,7 @@ public class CustomAuthFilter extends GenericFilterBean {
                 return;
             }
         } else {
-            log.info("토큰이 없는 요청 - resolveToken : {}, requestURI : {} ", customToken, requestURI);
+            log.info("토큰이 없는 요청");
         }
 
         filterChain.doFilter(request, response); // 다음 필터로 넘어가기 (토큰이 없는 경우는 로그인으로)
