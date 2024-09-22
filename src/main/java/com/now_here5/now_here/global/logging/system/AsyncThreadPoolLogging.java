a85b03a4dd3ff3ab42ce29fb83695832165b8e99
Huijun Seo <gjshj366@gmail.com>
2024-09-22 16:41:00 +0900

    Infra : 로깅 및 프로필 설정

--- src/main/java/com/now_here5/now_here/global/logging/system/AsyncThreadPoolLogging.java
+++ src/main/java/com/now_here5/now_here/global/logging/system/AsyncThreadPoolLogging.java
@@ -1,22 +1,31 @@
 package com.now_here5.now_here.global.logging.system;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Executor;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class AsyncThreadPoolLogging {
 
-    private final ThreadPoolTaskExecutor taskExecutor;
+    @Qualifier("asyncExecutor")
+    private final Executor asyncExecutor;
 
     public void logAsyncExecutorStatus() {
-        log.info("Async Pool - Active Threads: {}, Pool Size: {}, Queue Size: {}",
-                taskExecutor.getActiveCount(),
-                taskExecutor.getPoolSize(),
-                taskExecutor.getThreadPoolExecutor().getQueue().size());
+        if (asyncExecutor instanceof ThreadPoolTaskExecutor) {
+            ThreadPoolTaskExecutor taskExecutor = (ThreadPoolTaskExecutor) asyncExecutor;
+            log.info("Async Pool - Active Threads: {}, Pool Size: {}, Queue Size: {}",
+                    taskExecutor.getActiveCount(),
+                    taskExecutor.getPoolSize(),
+                    taskExecutor.getThreadPoolExecutor().getQueue().size());
+        } else {
+            log.warn("The provided Executor is not a ThreadPoolTaskExecutor.");
+        }
     }
 }
