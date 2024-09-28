4cf4a47275b1ef40abde8b4fbb8aab47522b907c
Huijun Seo <gjshj366@gmail.com>
2024-09-28 22:22:44 +0900

    Hotfix : dev 반영

--- src/main/java/com/now_here5/now_here/infra/notification/service/SmsService.java
+++ src/main/java/com/now_here5/now_here/infra/notification/service/SmsService.java
@@ -15,20 +15,15 @@ import org.springframework.stereotype.Service;
 public class SmsService {
 
     private final SmsSender smsSender;
-    private final SlackNotificationService slackNotificationService;
 
     @Autowired
-    public SmsService(@Qualifier("twilio") TwilioSmsSender smsSender, SlackNotificationService slackNotificationService) {
+    public SmsService(@Qualifier("twilio") TwilioSmsSender smsSender) {
         this.smsSender = smsSender;
-        this.slackNotificationService = slackNotificationService;
-        // 임시 방편
     }
 
     @Async
     public void sendSms(SmsRequest smsRequest) {
-        // Uncomment the line below to actually send the SMS
-        //        smsSender.sendSms(smsRequest);
-        slackNotificationService.sendNotification("SMS sent to " + smsRequest.getPhoneNumber() + " with message: " + smsRequest.getMessage());
+        smsSender.sendSms(smsRequest);
         log.info("Sending SMS to phone number: {}, with message: {}",
                 smsRequest.getPhoneNumber(),
                 smsRequest.getMessage());
