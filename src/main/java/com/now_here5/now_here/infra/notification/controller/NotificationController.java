d82bf6a2d3100e192fc6bc1fa346b787a2d075c5
Huijun Seo <gjshj366@gmail.com>
2024-09-28 21:51:10 +0900

    Dev : 개발용 sms -> slack 변경

--- src/main/java/com/now_here5/now_here/infra/notification/controller/NotificationController.java
+++ src/main/java/com/now_here5/now_here/infra/notification/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.now_here5.now_here.infra.notification.controller;
 
 import com.now_here5.now_here.infra.notification.dto.SmsRequest;
 import com.now_here5.now_here.infra.notification.service.SmsService;
+import com.now_here5.now_here.infra.slack.service.SlackNotificationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +20,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class NotificationController {
 
     private final SmsService smsService;
+    private final SlackNotificationService slackNotificationService;
 
     @PostMapping
     public void sendSms(@Valid @RequestBody SmsRequest smsRequest) {
-        smsService.sendSms(smsRequest);
+        // smsService.sendSms(smsRequest);
+        slackNotificationService.sendNotification("send to "+ smsRequest.getPhoneNumber() + " : " + smsRequest.getMessage());
     }
-
 }
